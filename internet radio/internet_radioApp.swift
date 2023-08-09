9c6c8a06183f17615082d01c60f89a1bb737ba67
Matus Barta <matus.barta64@gmail.com>
2023-08-09 15:08:17 +0200

    Initial Commit

---
+++ internet radio/internet_radioApp.swift
@@ -0,0 +1,17 @@
+//
+//  internet_radioApp.swift
+//  internet radio
+//
+//  Created by Matus Barta on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct internet_radioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
